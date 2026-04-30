7e287b8fc6edd884c360c505c2ee453bd0f5fcb7
Maheshkumar <mfmahesh.jmc@gmail.com>
2026-04-30 16:54:08 +0530

    Added Url mapping Model

---
+++ url-shortener-api/Models/UrlMapping.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace url_shortener_api.Models
+{
+    public class UrlMapping
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string OriginalUrl { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(10)]
+        public string ShortCode { get; set; } = string.Empty;
+
+        public bool IsPrivate { get; set; } = false;
+
+        public int ClickCount { get; set; } = 0;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
