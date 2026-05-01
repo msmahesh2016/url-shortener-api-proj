15034b57b5601717b491ed0d7cfd99c6b386ee6d
Maheshkumar <mfmahesh.jmc@gmail.com>
2026-05-01 15:04:59 +0530

    Create DB and table

---
+++ url-shortener-api/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using url_shortener_api.Models;
+
+namespace url_shortener_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+       : base(options) { }
+
+        public DbSet<UrlMapping> Urls => Set<UrlMapping>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UrlMapping>()
+                .HasIndex(x => x.ShortCode)
+                .IsUnique();
+        }
+    }
+}
---
+++ url-shortener-api/Migrations/20260501091356_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace url_shortener_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OriginalUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    ShortCode = table.Column<string>(type: "TEXT", maxLength: 10, nullable: false),
+                    IsPrivate = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ClickCount = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urls", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_ShortCode",
+                table: "Urls",
+                column: "ShortCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urls");
+        }
+    }
+}
