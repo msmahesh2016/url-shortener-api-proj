f5c36d3dde31a512d900f1d58465db1a78ee83e4
Maheshkumar <mfmahesh.jmc@gmail.com>
2026-05-01 15:08:26 +0530

    Post and Get url

--- url-shortener-api/Program.cs
+++ url-shortener-api/Program.cs
@@ -1,42 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using url_shortener_api.Data;
+using url_shortener_api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add DbContext (SQLite)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite")));
+
+var dbPath = @"C:\temp\urls.db";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite($"Data Source={dbPath}"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("allow", policy =>
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseCors("allow");
 
-app.MapGet("/weatherforecast", () =>
+// Apply migrations automatically
+using (var scope = app.Services.CreateScope())
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+string GenerateShortCode()
+{
+    const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var random = new Random();
+
+    return new string(Enumerable.Repeat(chars, 6)
+        .Select(s => s[random.Next(s.Length)]).ToArray());
+}
+
+
+async Task<string> GenerateUniqueCode(AppDbContext db)
+{
+    string code;
+    do
+    {
+        code = GenerateShortCode();
+    }
+    while (await db.Urls.AnyAsync(x => x.ShortCode == code));
+
+    return code;
+}
+
+
+app.MapPost("/api/url", async (AppDbContext db, UrlMapping req) =>
+{
+    req.ShortCode = await GenerateUniqueCode(db);
+    req.CreatedAt = DateTime.UtcNow;
+
+    db.Urls.Add(req);
+    await db.SaveChangesAsync();
+
+    return Results.Ok(req);
+});
+
+app.MapGet("/api/url/public", async (AppDbContext db) =>
+{
+    return await db.Urls
+        .Where(x => !x.IsPrivate)
+        .OrderByDescending(x => x.CreatedAt)
+        .ToListAsync();
+});
+
+
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
